ec19429c7140ccc6cffff7c3f1011b1ed397919a
ZvDaniil <119564912+ZvDaniil@users.noreply.github.com>
2023-05-17 11:56:28 +0700

    Dependency Injection

---
+++ DE.Persistence/Configurations/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using DE.Application.Interfaces;
+
+namespace DE.Persistence.Configurations;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration["DbConnection"];
+
+        services
+            .AddDbContext<ApplicationDbContext>(options => options.UseSqlite(connectionString))
+            .AddScoped<IApplicationDbContext>(sp => sp.GetRequiredService<ApplicationDbContext>());
+
+        return services;
+    }
+}
