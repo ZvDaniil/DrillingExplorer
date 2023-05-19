23a74272b9bfae2f74b9d72f36b03966625cd199
ZvDaniil <119564912+ZvDaniil@users.noreply.github.com>
2023-05-19 17:10:21 +0700

    Npgsql

--- DE.Persistence/Configurations/DependencyInjection.cs
+++ DE.Persistence/Configurations/DependencyInjection.cs
@@ -10,7 +10,7 @@ public static class DependencyInjection
     public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseSqlite(configuration.GetConnectionString("Sqlite")));
+            options.UseNpgsql(configuration.GetConnectionString("Npgsql")));
 
         services.AddScoped<IApplicationDbContext>(provider =>
             provider.GetRequiredService<ApplicationDbContext>());
